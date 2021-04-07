cb55929f6dd4a9d72c22c4884c429069958833a1
Benoît Rocco <benoit.rocco@gmail.com>
2021-04-07 21:20:27 +0200

    https binding

--- run-release.sh
+++ run-release.sh
@@ -1,2 +1,3 @@
 # Démarre l'application en bindant toutes les adresses
-dotnet bin/Release/net5.0/publish/wind-forecast-api.dll --urls http://0.0.0.0:5000
+dotnet bin/Release/net5.0/publish/wind-forecast-api.dll --urls https://0.0.0.0:5001
+#dotnet bin/Release/net5.0/publish/wind-forecast-api.dll --urls http://0.0.0.0:5000
