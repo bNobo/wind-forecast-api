5282e5602acb7ca10ea22f09809e2ac4314cb270
bNobo <benoit.rocco@gmail.com>
2023-09-29 19:52:02 +0200

    remove deprecated files
    le déploiement se fait maintenant grace aux github actions.

--- publish.sh
+++
@@ -1,5 +0,0 @@
-# Attention : aucune variable d'environnement n'est encore définie sur le serveur, il faut tout mettre dans appsettings.json
-dotnet publish --configuration Release
-ssh pi@192.168.1.25 sudo systemctl stop windforecast-api
-scp -r ./bin/Release/net5.0/* pi@192.168.1.25:/home/pi/windforecast-api/
-ssh pi@192.168.1.25 sudo systemctl start windforecast-api
--- run-release.sh
+++
@@ -1,3 +0,0 @@
-# Démarre l'application en bindant toutes les adresses
-dotnet bin/Release/net5.0/publish/wind-forecast-api.dll --urls https://0.0.0.0:5001
-#dotnet bin/Release/net5.0/publish/wind-forecast-api.dll --urls http://0.0.0.0:5000
